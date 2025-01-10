4c72b2bb7935b8174966ca7e92c8d3192d39f57a
Victor Fondevilla <victor@fondevilla.io>
2025-01-10 08:34:57 +0100

    feat: First release into the dedicated repo

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      security.pam.enableSudoTouchIdAuth = true;
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      # nixpkgs.hostPlatform = "x86_64-darwin";
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      system.defaults = {
+        dock = {
+          autohide = true;
+          mru-spaces = false;
+        };
+        finder = {
+          AppleShowAllFiles = true;
+          FXPreferredViewStyle = "Nlsv";
+          ShowPathbar = true;
+          FXDefaultSearchScope = "SCcf";
+          AppleShowAllExtensions = true;
+        };
+        
+        NSGlobalDomain = {
+          "com.apple.swipescrolldirection" = false;
+          AppleEnableSwipeNavigateWithScrolls = true;
+        };
+        NSGlobalDomain."com.apple.trackpad.trackpadCornerClickBehavior" = 1;
+
+        trackpad.Clicking = true;
+        trackpad.TrackpadRightClick = true;
+      };
+    };
+
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Victors-MacBook-Pro
+    darwinConfigurations."base" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."base".pkgs;
+  };
+}
